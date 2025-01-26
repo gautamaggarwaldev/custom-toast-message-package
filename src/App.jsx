7a42dcce013db9da423abbd0909e01fe0bd33ddc
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-01-26 18:14:11 +0530

    finalize the code with toast component

--- src/App.jsx
+++ src/App.jsx
@@ -1,123 +1,192 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import { v4 as uuid } from 'uuid';
-import Toast from './Toast Component/Toast';
+import React, { useState } from "react";
+import Toast from "./Toast Component/Toast";
+import { v4 as uuid } from "uuid";
 
 const App = () => {
-
-  // state for toast message, position
   const [data, setData] = useState({
-    message: "", 
-    checked: false, // initially unchecked ----> auto dismiss
-    position: "top-center",
-    duration: null, // duration for clear the toast message ----> null (disable)
+    message: "",
+    checked: false, // initially unchecked
+    duration: null, // disabled
+    position: "bottom-right", //default position
   });
 
-  //toast list 
+  // for toasts list
   const [toastList, setToastList] = useState({
-    "top-center": [],
     "bottom-right": [],
     "bottom-left": [],
     "top-right": [],
-    "top-left":[],
+    "top-left": [],
+    "top-center": [],
   });
 
-  //handle user input change function
-  function handleChange(event) {
+  // function to handle the input change
+  const handleChange = (event) => {
     const { name, value } = event.target;
-    const newData = {...data};
-    console.log("newData ---> ", newData);
+    const newData = { ...data };
     newData[name] = value;
-    console.log("newData[name] ---> ", value);
-    setData({...newData});
-  }
+    setData({ ...newData });
+  };
 
-  //function to add new toast message
-  const addToastMessage = ({ type }) => {
+  // function to add toast
+  const addNewToast = (type) => {
     const newData = { ...toastList };
-    const currentPosition = data?.position; // if data is present then set the position to the current position
+    const currentPostion = data?.position;
 
-    //create new toast
+    // creating new toast
     const newToast = {
       id: uuid(),
-      message: data?.message ? data?.message : type === "success" ? "Data fetched successfully" : type==="error" ? "Internal server error" : "Invalid data provided",
+      position: currentPostion,
+      message: data?.message
+        ? data?.message
+        : type === "success"
+        ? "Data fetched successfully"
+        : type === "error"
+        ? "Internal server error"
+        : "Invalid data provided",
       duration: data?.checked ? (data?.duration ? data?.duration : 1) : null,
       type,
     };
 
-    newData[currentPosition].push(newToast);
-    setData({ ...newData });
+    newData[currentPostion].push(newToast);
+    setToastList({ ...newData });
 
-
-    //set-timeout for auto delete the toast package
-    if(newToast.duration) {
+    // adding set timeout for auto delete
+    if (newToast.duration) {
       setTimeout(() => {
         setToastList((prevToastList) => {
-          const updatedToastData = {... prevToastList};
-          updatedToastData[currentPosition] = updatedToastData[currentPosition].filter((item) => item.id !== newToast.id);
-          return {...updatedToastData};
+          const updatedData = { ...prevToastList };
+          updatedData[currentPostion] = updatedData[currentPostion].filter(
+            (item) => item.id !== newToast.id
+          );
+          return { ...updatedData };
         });
-      }, newToast.duration * 1000); //seconds
+      }, newToast.duration * 1000);
     }
-  }
-
+  };
 
   return (
-    <div className='bg-blue-200'>
-      <div>
-        <h1 className='text-6xl font-extrabold font-serif text-center mt-10'>Toasty JS</h1>
-        <header className='flex justify-center mt-25 gap-60'>
-          <div className='flex gap-5'>
-            <input type='checkbox' id='checked' name='checked' className='cursor-pointer' checked={data?.checked} onChange={() => { setData({ ...data, checked: !data?.checked }) }} />
-            <label className='mt-3.5' htmlFor='checked'>Auto-Dismiss</label>
+    <div className="flex items-center justify-center h-screen font-semibold bg-gray-50">
+      {/* container for toast card options */}
+      <div className="p-5 space-y-5 text-teal-600 rounded-md shadow-md">
+        <h1 className="text-2xl font-bold text-center ">
+          Custom React Toast Package
+        </h1>
+
+        {/* for auto dismiss and duration */}
+        <header className="grid grid-cols-2 gap-5">
+          {/* for auto dismiss */}
+          <div className="flex items-center self-start gap-2">
+            <input
+              type="checkbox"
+              id="checked"
+              name="checked"
+              className="cursor-pointer"
+              checked={data?.checked}
+              onChange={() => {
+                setData({ ...data, checked: !data?.checked });
+              }}
+            />
+            <label htmlFor="checked" className="mb-1 cursor-pointer">
+              Auto-dismiss
+            </label>
           </div>
 
-          <div className='flex flex-col gap-2.5'>
-            <label>Duration (in sec)</label>
-            <input type='number' placeholder='5' id='duration' name='duration' disabled={!data?.checked} defaultValue={!data?.checked ?  1 : null} onChange={(event) => handleChange(event)} />
+          {/* for duration */}
+          <div className="flex flex-col gap-2 mb-1">
+            <label htmlFor="duration">Duration (in second)</label>
+            <input
+              type="number"
+              placeholder="5"
+              id="duration"
+              name="duration"
+              disabled={!data?.checked}
+              defaultValue={!data?.checked ? 1 : null}
+              className=" border-[1.5px] border-teal-600 outline-none p-1 rounded-md"
+              onChange={(event) => handleChange(event)}
+            />
           </div>
         </header>
 
-        <main className='flex justify-center mr-20 mt-5'>
-          <div className='flex flex-col'>
-            <label htmlFor='message'>Toast Message</label>
-            <input className='mt-4' name='message' type='text' placeholder='Data fetched successfully' value={data?.message} onChange={(event) => handleChange(event)} />
+        {/* for position and message */}
+        <main className="grid grid-cols-2 gap-5">
+          {/* for message */}
+          <div className="flex flex-col gap-1">
+            <label htmlFor="message">Toast message</label>
+            <input
+              name="message"
+              type="text"
+              placeholder="Data fetched"
+              value={data?.message}
+              onChange={(event) => handleChange(event)}
+              className="border-[1.5px] border-teal-600 outline-none p-1 rounded-md"
+            />
           </div>
 
-          <div className='flex flex-col ml-45'>
-            <label htmlFor='position'>Toast Position</label>
-            <select className='mt-4' name='position' id='position' onChange={(event) => handleChange(event)}>
-              <option value='top-center'>Top Center</option>
-              <option value='top-left'>Top Left</option>
-              <option value='top-right'>Top Right</option>
-              <option value='bottom-right'>Bottom Right</option>
-              <option value='bottom-left'>Bottom Left</option>
+          {/* for position */}
+          <div className="flex flex-col gap-1">
+            <label htmlFor="position">Toast Position</label>
+            <select
+              name="position"
+              id="position"
+              className="border-[1.5px] border-teal-600 outline-none p-1 rounded-md"
+              onChange={(event) => handleChange(event)}
+            >
+              <option value="bottom-right">Bottom Right</option>
+              <option value="bottom-left">Bottom Left</option>
+              <option value="top-left">Top Left</option>
+              <option value="top-right">Top Right</option>
+              <option value="top-center">Top Center</option>
             </select>
           </div>
         </main>
 
-        <footer>
-          <div className='grid grid-cols-2 gap-y-5'>
-            <button onClick={() => addToastMessage("success")}>Success Toast</button>
-            <button onClick={() => addToastMessage("error")}>Error Toast</button>
-            <button onClick={() => addToastMessage("warning")}>Warning Toast</button>
-            <button 
-              onClick={() => setToastList({
-                "top-center": [],
+        {/* for footer */}
+        <footer className="grid grid-cols-2 gap-x-5 gap-y-2">
+          {/* success button */}
+          <button
+            className="py-2 text-white bg-teal-600 rounded-md hover:bg-teal-700"
+            onClick={() => addNewToast("success")}
+          >
+            Success Toast
+          </button>
+
+          {/* error button */}
+          <button
+            className="py-2 text-white bg-teal-600 rounded-md hover:bg-teal-700"
+            onClick={() => addNewToast("error")}
+          >
+            Error Toast
+          </button>
+
+          {/* warning button */}
+          <button
+            className="py-2 text-white bg-teal-600 rounded-md hover:bg-teal-700"
+            onClick={() => addNewToast("warn")}
+          >
+            Warning Toast
+          </button>
+
+      
+          {/* clear all button */}
+          <button
+            className="py-2 text-white bg-teal-600 rounded-md hover:bg-teal-700"
+            onClick={() =>
+              setToastList({
                 "bottom-right": [],
                 "bottom-left": [],
                 "top-right": [],
-                "top-left":[],
-              })} >Clear All</button>
-          </div>
+                "top-left": [],
+                "top-center": [],
+              })
+            }
+          >
+            Clear All Toast
+          </button>
         </footer>
-        <div>
-          <Toast />
-        </div>
       </div>
+      <Toast toastList={toastList} setToastList={setToastList} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
